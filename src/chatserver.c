de98c2d4ec6986b9da84e59770b17237ebc1dfee
SelenaSmall <selenawiththetattoo@gmail.com>
2014-06-16 20:05:49 +1200

    can select all  menus and sub menus forward and backward - case 4 options_menu oddly stopped working

--- src/chatserver.c
+++ src/chatserver.c
@@ -29,8 +29,8 @@ void options_menu( int s_id) {
 	char buffer[1024];
 
 	print_socket(s_id, "Please select an option:\n"); 
-	print_socket(s_id, "1: See the Menu \n");
-	print_socket(s_id, "2: Make and Order\n");
+	print_socket(s_id, "1: See the Drinks Menu \n");
+	print_socket(s_id, "2: Make an Order\n");
 	print_socket(s_id, "3: See how much I've spent\n");
 	print_socket(s_id, "4: Too hard, I quit\n");
 
@@ -42,10 +42,10 @@ void options_menu( int s_id) {
 		int command = atoi(buffer);
 		switch(command) {
 			case 1: 
-				print_menu_1(s_id);
+				print_menu_drinks(s_id);
 				break;
 			case 2: 
-				print_menu_2(s_id);
+				print_menu_order(s_id);
 				break;
 			case 3: 
 				print_socket(s_id, "Total: \n");
@@ -60,14 +60,15 @@ void options_menu( int s_id) {
 	}
 }
 
-void print_menu_1(int s_id) {
-	char buffer[1024];
+void print_menu_drinks(int s_id) { /* needs to be seperate loop from above*/
+	char buffer[1024]; 
 
 	print_socket(s_id, "1: Beer \n");
 	print_socket(s_id, "2: Wine \n");
 	print_socket(s_id, "3: Cocktails \n");
+	print_socket(s_id, "4: Go Back \n");
 
-	int running = 1;
+	int running = 2;
 	while(running) {
 		print_socket(s_id, ">");
 
@@ -84,7 +85,110 @@ void print_menu_1(int s_id) {
 				print_cocktail_options(s_id);
 				break;
 			case 4: 
-				running = 0;
+				options_menu(s_id); /*should return to print options menu */
+				break;
+			default:
+				print_socket(s_id, "That is not an option!\n");
+				break; 
+		}
+	} 
+}
+
+void print_beer_options(int s_id) {
+	char buffer[1024];
+
+	print_socket(s_id, "1: Pilsner \n");
+	print_socket(s_id, "2: IPA \n");
+	print_socket(s_id, "3: Pale Ale \n");
+	print_socket(s_id, "4: Dark Ale \n");
+	print_socket(s_id, "5: Blonde Ale \n");
+	print_socket(s_id, "6: Go Back \n");
+
+	int running = 3;
+	while(running) {
+		print_socket(s_id, ">");
+
+		gets_socket(s_id, (char*)&buffer);
+		int command = atoi(buffer);
+		switch(command) {
+			case 1: 
+				print_socket(s_id, "Pilsner\n");
+				break;
+			case 2: 
+				print_socket(s_id, "IPA\n");
+				break;
+			case 3: 
+				print_socket(s_id, "Pale Ale\n");
+				break;
+			case 4: 
+				print_socket(s_id, "Dark Ale\n");
+				break;
+			case 5: 
+				print_socket(s_id, "Blonde Ale\n");
+				break;
+			case 6: 
+				print_menu_drinks(s_id); /*should return to print options menu - it does but its not a loop and doesnt say anything*/
+				break;
+			default:
+				print_socket(s_id, "That is not an option!\n");
+				break;
+		}
+	}
+
+}
+
+void print_wine_options(int s_id) {
+	char buffer[1024];
+
+	print_socket(s_id, "1: Red Wine \n");
+	print_socket(s_id, "2: White Wine \n");
+ 	print_socket(s_id, "3: Go Back \n");
+
+ 	int running = 3;
+	while(running) {
+		print_socket(s_id, ">");
+
+		gets_socket(s_id, (char*)&buffer);
+		int command = atoi(buffer);
+		switch(command) {
+			case 1: 
+				print_socket(s_id, "Red Wine\n");
+				break;
+			case 2: 
+				print_socket(s_id, "White Wine\n");
+				break;
+			case 3: 
+				print_menu_drinks(s_id);
+				break;
+			default:
+				print_socket(s_id, "That is not an option!\n");
+				break;
+		}
+	}	
+}
+
+void print_cocktail_options(int s_id) {
+	char buffer[1024];
+
+	print_socket(s_id, "1: Martini \n");
+	print_socket(s_id, "2: Margarita \n");
+	print_socket(s_id, "3: Go Back \n");
+
+	int running = 3;
+	while(running) {
+		print_socket(s_id, ">");
+
+		gets_socket(s_id, (char*)&buffer);
+		int command = atoi(buffer);
+		switch(command) {
+			case 1: 
+				print_socket(s_id, "Martini\n");
+				break;
+			case 2: 
+				print_socket(s_id, "Margarita\n");
+				break;
+			case 3: 
+				print_menu_drinks(s_id);
 				break;
 			default:
 				print_socket(s_id, "That is not an option!\n");
@@ -93,28 +197,32 @@ void print_menu_1(int s_id) {
 	}
 }
 
-void print_beer_options(int s_id) {
-	print_socket(s_id, "1: Pilsner \n");
-	print_socket(s_id, "2: IPA \n");
-	print_socket(s_id, "3: Pale Ale \n");
-	print_socket(s_id, "4: Dark Ale \n");
-	print_socket(s_id, "5: Blonde Ale \n");
-	print_socket(s_id, "6: Go Back \n");
-}
+void print_menu_order(int s_id) {
+	char buffer[1024];
 
-void print_wine_options(int s_id) {
-	print_socket(s_id, "1: Red Wine \n");
-	print_socket(s_id, "2: White Wine \n");
-	print_socket(s_id, "3: Go Back \n");	
-}
-
-void print_cocktail_options(int s_id) {
-	print_socket(s_id, "1: Martini \n");
-	print_socket(s_id, "2: Margarita \n");
-	print_socket(s_id, "3: Go Back \n");
-}
-
-void print_menu_2(int s_id) {
 	print_socket(s_id, "1: Food \n");
 	print_socket(s_id, "2: Drinks \n");
+	print_socket(s_id, "2: Go Back \n");
+
+	int running = 3;
+	while(running) {
+		print_socket(s_id, ">");
+
+		gets_socket(s_id, (char*)&buffer);
+		int command = atoi(buffer);
+		switch(command) {
+			case 1: 
+				print_socket(s_id, "Food\n");
+				break;
+			case 2: 
+				print_socket(s_id, "Drinks\n");
+				break;
+			case 3: 
+				print_menu_drinks(s_id);
+				break;
+			default:
+				print_socket(s_id, "That is not an option!\n");
+				break;
+		}
+	}		
 }
--- src/chatserver.h
+++ src/chatserver.h
@@ -4,9 +4,9 @@
 
 void options_menu( int s_id);
 
-void print_menu_1(int s_id);
+void print_menu_drinks(int s_id);
 
-void print_menu_2(int s_id);
+void print_menu_order(int s_id);
 
 void print_beer_options(int s_id);
 
